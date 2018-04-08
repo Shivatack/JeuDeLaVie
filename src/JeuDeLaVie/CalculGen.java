85b0c0575e7ec66f81c81f243ed94c4ccc201a2b
UzayUmutSag <35768292+UzayUmutSag@users.noreply.github.com>
2018-04-08 14:52:56 +0200

    Uzay
    New calculGen

--- src/JeuDeLaVie/CalculGen.java
+++ src/JeuDeLaVie/CalculGen.java
@@ -1,64 +1,8 @@
 package JeuDeLaVie;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class CalculGen {
-
-//Ayoub et Juliette
-
-    /**
-     * Fonction qui exécute une simulation du jeu d’une durée d affichant les configurations du jeu avec le numéro de génération.
-     *
-     * @param d : nombre de generations
-     * @param l : liste fournie par la lecture du .lif
-     */
-    public static Liste<Coordonnee> simulation(int d, Liste<Coordonnee> l){
-        int compteur = 0;
-        //d++;
-        Liste<Coordonnee> v;
-        while (compteur != d){
-            System.out.println(l.toString());
-            System.out.println("\n Génération n° "+compteur);
-            compteur++;
-            v = listeVoisins(l);
-            l = genSuivante(v, l);
-        }
-        return l;
-    }
-
-    // Juliette et Ayoub
-
-    /**
-     * Fonction qui prend une liste de cellules vivantes en paramètre et qui renvoie une liste de cases avec leur nombre de cellules vivantes adjacentes
-     *
-     * @param : liste l de cellules
-     * @return : liste v de nombre de voisins
-     */
-    public static Liste listeVoisins(Liste<Coordonnee> l ) { // recoit la liste l qui contient les cellules vivantes de la génération actuelle
-
-        Liste<Coordonnee> v = new Liste<Coordonnee>(); // Liste de voisins a retourner
-        Maillon<Coordonnee> tmp = l.getTete(); // Maillon servant a parcourir la liste l
-
-        while (tmp != null) {
-            int ligne = tmp.getInfo().getLigne();
-            int colonne = tmp.getInfo().getColonne();
-
-            for (int i = ligne - 1; i <= ligne + 1; i++) {
-                for (int j = colonne - 1; j <= colonne + 1; j++) {
-                    Coordonnee c = new Coordonnee(i, j, 1);
-                    Maillon<Coordonnee> m = new Maillon<Coordonnee>(c, null);
-                    if (!tmp.getInfo().equals(m.getInfo())) { //bien
-                        if (v.contient(m)) {
-                            ajouterUnVoisin(v, m);
-                        } else {
-                            v.ajouterMaillon(m);
-                        }
-                    }
-                }
-            }
-            tmp = tmp.getSuivant();
-        }
-
-        return v;
-    }
 
     // Juliette et Ayoub
 
@@ -102,4 +46,135 @@ public class CalculGen {
         }
         return v;
     }
+
+    public static Liste listeVoisins(Liste<Coordonnee> l , int monde , int plin  , int pcol , int glin , int gcol) {
+
+        Liste<Coordonnee> v = new Liste<Coordonnee>(); // Liste de voisins a retourner
+        Maillon<Coordonnee> tmp = l.getTete(); // Maillon servant a parcourir la liste l
+
+        while (tmp != null) {
+            int ligne = tmp.getInfo().getLigne();
+            int colonne = tmp.getInfo().getColonne();
+
+            int[][] t =remplir_tabs(monde , ligne , colonne , plin , pcol , glin , gcol);
+
+            for (int i : t[0]) {
+                for (int j : t[1]) {
+                    Coordonnee c = new Coordonnee(i, j, 1);
+                    Maillon<Coordonnee> m = new Maillon<Coordonnee>(c, null);
+                    if (!tmp.getInfo().equals(m.getInfo())) { //bien
+                        if (v.contient(m)) {
+                            ajouterUnVoisin(v, m);
+                        } else {
+                            v.ajouterMaillon(m);
+                        }
+                    }
+                }
+            }
+            tmp = tmp.getSuivant();
+        }
+        return v;
+    }
+
+    public static Liste<Coordonnee> simulation(int d, Liste<Coordonnee> l,int monde){
+        int compteur = 0;
+        Liste<Coordonnee> v;
+        int plin=JeuDeLaVie.minLigne(l);
+        System.out.println(plin);
+        int glin=JeuDeLaVie.maxLigne(l);
+        System.out.println(glin);
+        int pcol=JeuDeLaVie.minColonne(l);
+        System.out.println(pcol);
+        int gcol=JeuDeLaVie.maxColonne(l);
+        System.out.println(gcol);
+        while (compteur != d){
+            System.out.println(l.toString());
+            System.out.println("\n Génération n° "+compteur);
+            compteur++;
+            v = listeVoisins(l,monde,plin,pcol,glin,gcol);
+            System.out.println("###################");
+            l = genSuivante(v, l);
+        }
+        return l;
+    }
+
+    /*
+    1->infini
+    2->fini
+    3->circulaire
+     */
+    public static int[][] remplir_tabs(int monde , int ligne , int colonne , int plin  , int pcol , int glin , int gcol){
+        int [] t_l;
+        int [] t_c;
+        if(monde==1){
+            t_l = new int[3];
+            t_c = new int[3];
+            t_l[0]=ligne-1;
+            t_l[1]=ligne;
+            t_l[2]=ligne+1;
+            t_c[0]=colonne-1;
+            t_c[1]=colonne;
+            t_c[2]=colonne+1;
+        }else if(monde==2){
+            if (ligne == plin) {
+                t_l = new int[2];
+                t_l[0] = ligne + 1;
+                t_l[1] = ligne;
+            } else if (ligne == glin) {
+                t_l = new int[2];
+                t_l[0] = ligne - 1;
+                t_l[1] = ligne;
+            } else {
+                t_l = new int[3];
+                t_l[0] = ligne - 1;
+                t_l[1] = ligne;
+                t_l[2] = ligne + 1;
+            }
+            if (colonne == pcol) {
+                t_c = new int[2];
+                t_c[0] = colonne + 1;
+                t_c[1] = colonne;
+            } else if (colonne == gcol) {
+                t_c = new int[2];
+                t_c[0] = colonne - 1;
+                t_c[1] = colonne;
+            } else {
+                t_c = new int[3];
+                t_c[0] = colonne - 1;
+                t_c[1] = colonne;
+                t_c[2] = colonne + 1;
+            }
+        }else{
+            t_l = new int[3];
+            t_c = new int[3];
+            if (ligne == plin) {
+                t_l[0]=ligne + 1;
+                t_l[1]=ligne;
+                t_l[2]=glin;
+            } else if (ligne == glin) {
+                t_l[0]=ligne - 1;
+                t_l[1]=ligne;
+                t_l[2]=plin;
+            } else {
+                t_l[0]=ligne - 1;
+                t_l[1]=ligne;
+                t_l[2]=ligne + 1;
+            }
+            if (colonne == pcol) {
+                t_c[0]=colonne + 1;
+                t_c[1]=colonne;
+                t_c[2]=gcol;
+            } else if (colonne == gcol) {
+                t_c[0]=colonne - 1;
+                t_c[1]=colonne;
+                t_c[2]=pcol;
+            } else {
+                t_c[0]=colonne-1;
+                t_c[1]=colonne;
+                t_c[2]=colonne+1;
+            }
+        }
+        return new int[][] {t_l, t_c};
+    }
+
 }
